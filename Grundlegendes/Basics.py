8ff46a7480557e335668f6e39cc6357ccf31ed94
DeRocketman <De.Rocketman@outlook.com>
2021-02-12 12:50:50 +0100

    Rocket: for Schleife (mit Rangefunktion) sowie Funktionen hinzugefügt

--- Grundlegendes/Basics.py
+++ Grundlegendes/Basics.py
@@ -34,7 +34,7 @@ else:
 
 # Ein Bot soll mal raten :-)
 
-print("Nun bin ich, der Ratebot, mal dran:")
+print("Nun bin ich, der Rate-Bot, mal dran:")
 zaehler2 = 0
 geheimnis2 = int(input("Gib eine Zahl ein, die ich erraten soll "))
 obereSchranke = int(input("Gib eine Zahl als obere Begrenzung ein(Muss größer oder gleich der gesuchten Zahl sein!) "))
@@ -68,3 +68,34 @@ if zaehler2 == 1:
     print("WOW beim 1. Versuch gleich getroffen. Ich sollte mal Lotto spielen. Aber mit welchen Zahlen?")
 else:
     print("Supidupi, ich haben es in", zaehler2, "Versuchen geschafft! Unfassbar!")
+
+# for - Schleife
+x = ["Python", "Java", "JavaScript"]
+
+for w in x:
+    print(w)
+print("FERTIG")
+
+# for - Schleife als Zählschleife
+for i in range(5):
+    print(i)
+
+
+# zählt von 0-5, auch möglich start und ende oder mit Schrittgröße range(start, ende, schritt)
+
+# Funktionen mit keyword def (definition) und Funktionsbezeichnung
+def funk():
+    for j in range(0, 20):
+        print(j)
+
+
+# Funktionen mit Parameter
+
+def funkParam(n, m, s):
+    for p in range(n, m, s):
+        print(p)
+
+
+# Funktionsaufruf
+funk()
+funkParam(0, 10000000, 2)
